2a41fd8b4da312d6a9550119cce649d38d3e63f4
ChemiKyle <Kyle.2493@gmail.com>
2018-07-09 17:37:41 -0400

    rpi draw to screen

---
+++ rpi/display_quotes.py
@@ -0,0 +1,57 @@
+import epd7in5b
+import Image
+import ImageDraw
+import ImageFont
+#import imagedata
+import random
+#import pickle
+import json
+import textwrap
+import os
+
+EPD_WIDTH = 640
+EPD_HEIGHT = 384
+
+def main():
+    os.chdir('/home/pi/epaper/mine')
+    author, title, quote = select_quote()
+#    print(quote)
+    draw(author, title, quote)
+
+def select_quote(file = '/home/pi/epaper/mine/quotes.json', use_json=True):
+    if use_json:
+        with open(file, 'r') as fp:
+            dic = json.load(fp)
+    else:
+        with open(file, 'rb') as fp:
+            dic = pickle.load(fp)
+    author, book_quotes = random.choice(list(dic.items()))
+    book = random.choice(list(book_quotes.items()))
+    title = book[0]
+    quote = random.choice(list(book[1].values()))
+
+    return author, title, quote
+
+def draw(author = "Albert Camus", title = "The Myth of Sisyphus",
+        quote = "The struggle itself towards the heights is enough to fill a man's heart. One must imagine Sisyphus happy."):
+    epd = epd7in5b.EPD()
+    epd.init()
+
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMonoBold.ttf', 20)
+    
+    #draw.rectangle((5, 5, 240, 384), fill = 0)
+
+    def wrap_text(text, width, margin, offset, fill=0):
+        for line in textwrap.wrap(text, width = 53):
+            draw.text((margin, offset), line, font=font, fill=fill) # quote
+            offset += font.getsize(line)[1]
+    wrap_text(quote, 53, 5, 5)
+    wrap_text('-{} "{}"'.format(author, title), 48, 30, 270)
+
+    image = image.rotate(180)
+    epd.display_frame(epd.get_frame_buffer(image))
+
+if __name__ == '__main__':
+    main()
