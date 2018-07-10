82ae19a97a2905f3f96699d2d828f4e2e175258b
ChemiKyle <kyle.2493@gmail.com>
2018-07-09 22:36:23 -0400

    Added SQLite storage with duplicate protection

--- scrape_quotes.py
+++ scrape_quotes.py
@@ -1,6 +1,8 @@
+#!/bin/python3
+
 def main():
     quotes = import_from_txt('all_kindle_notes.txt')
-    dic = scrape_info(quotes, True)
+    scrape_info(quotes)
 
 
 def import_from_txt(quote_file):
@@ -10,6 +12,7 @@ def import_from_txt(quote_file):
         quotes = f.split('==========')
     return quotes
 
+
 def split_quote(quote_block):
     splt = quote_block.split('\n')
     book, author = splt[1].split('(') # breaks if parentheses in title
@@ -17,15 +20,16 @@ def split_quote(quote_block):
     quote = splt[4]
     return book, author, quote
 
-def scrape_info(quotes, use_dict = False):
-    if use_dict:
-        dic = {}
-        use_json = True
-    i = 0
+
+def scrape_info(quotes, use_dict = False, use_json = False):
+    dic = {}
+    if not use_dict:
+        import sqlite3
+        conn = sqlite3.connect('data/quotes.db')
+        conn.execute('CREATE TABLE IF NOT EXISTS quotes(author, book, quote, '
+        'CONSTRAINT quote_unique UNIQUE (quote))')
+        c = conn.cursor()
     for quote_block in quotes[1:-1]:
-        i += 1
-        print(i)
-        print(quote_block)
         book, author, quote = split_quote(quote_block)
         if use_dict:
             if author not in dic:
@@ -36,6 +40,16 @@ def scrape_info(quotes, use_dict = False):
             else:
                 q = {'quote ' + str(len(dic[author][book]) + 1): quote}
                 dic[author][book].update(q)
+        else:
+            cmd =("INSERT INTO quotes(author,book,quote) "
+                    "VALUES(?,?,?)")
+            try:
+                c.execute(cmd, [author, book, quote])
+            except sqlite3.IntegrityError:
+                print("Skipping duplicate quote: {}".format(quote))
+    if not use_dict:
+        conn.commit()
+        conn.close()
     if use_json:
         import json
         with open('data/quotes.json', 'w') as fp:
@@ -46,7 +60,6 @@ def scrape_info(quotes, use_dict = False):
             pickle.dump(dic, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-
-
 if __name__ in '__main__':
     main()
+
